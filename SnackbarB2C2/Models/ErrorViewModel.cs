811df520cee23de62d9553f2397ce3af8e13d4f7
Megalomizer <Levifunk01@gmail.com>
2023-09-22 20:09:30 +0200

    First Creation of MVC project

---
+++ SnackbarB2C2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SnackbarB2C2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
